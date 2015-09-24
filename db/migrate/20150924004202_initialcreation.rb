964b2dd4d4a017ab4305eee50d889d9633b72a01
ruxandrafed <ruxandra.fediuc@gmail.com>
2015-09-24 12:02:42 -0700

    clobbered with migation from master

--- db/migrate/20150924004202_initialcreation.rb
+++ db/migrate/20150924004202_initialcreation.rb
@@ -22,44 +22,44 @@ class Initialcreation < ActiveRecord::Migration
       t.timestamps null: false
     end
 
-    create_table :test_results do |t|
-      t.references :test
-      t.string :candidate_name, null: false
-      t.string :candidate_email, null: false
-      t.string :candidate_score, null: false
-      t.timestamps null: false
-    end
+  create_table :test_results do |t|
+    t.references :test
+    t.string :candidate_name, null: false
+    t.string :candidate_email, null: false
+    t.string :candidate_score, null: false
+    t.timestamps null: false
+  end
 
-    create_table :questions do |t|
-      t.references :user
-      t.string :content, null: false
-      t.string :image
-      t.timestamps null: false
-    end
+  create_table :questions do |t|
+    t.references :user
+    t.string :content, null: false
+    t.string :image
+    t.timestamps null: false
+  end
 
-    create_table :question_tags do |t|
-      t.references :question
-      t.references :tag
-      t.timestamps null: false
-    end
+  create_table :question_tags do |t|
+    t.references :question
+    t.references :tag
+    t.timestamps null: false
+  end
 
-    create_table :tags do |t|
-      t.string :name
-      t.timestamps null: false
-    end
+  create_table :tags do |t|
+    t.string :name
+    t.timestamps null: false
+  end
 
-    create_table :answers do |t|
-      t.references :test
-      t.string :content, null: false
-      t.boolean :correct, null: false
-      t.timestamps null: false
-    end
+  create_table :answers do |t|
+    t.references :test
+    t.string :content, null: false
+    t.boolean :correct, null: false
+    t.timestamps null: false
+  end
 
-    create_table :ratings do |t|
-      t.references :question
-      t.references :user
-      t.integer :value
-      t.timestamps null: false
-    end
+  create_table :ratings do |t|
+    t.references :question
+    t.references :user
+    t.integer :value
+    t.timestamps null: false
+  end
   end
 end
