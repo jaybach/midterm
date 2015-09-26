e28b9f8ee56bf66c165a9c24a1eb6b1438fa3bd9
ruxandrafed <ruxandra.fediuc@gmail.com>
2015-09-25 19:55:04 -0700

    Minor fixes

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,15 +1,17 @@
 class Question < ActiveRecord::Base
 
   belongs_to :user
-  has_many :answers
-  has_many :question_selections
+  has_many :answers, dependent: :destroy
+  has_many :question_selections, dependent: :destroy
   has_many :tests, :through => :question_selections
-  has_many :question_tags
+  has_many :question_tags, dependent: :destroy
   has_many :tags, through: :question_tags
   has_many :ratings
   has_many :answers
 
   validates :content, length: { minimum: 5, maximum: 200 }
+
+  after_destroy :delete_question_selections, :delete_question_tags, :delete_question_answers
 
   def average_rating
     self.ratings.average(:value).to_f.round(2)
