c66361c34da4fb972a9271c23b577cb6af70fc3f
ruxandrafed <ruxandra.fediuc@gmail.com>
2015-09-25 21:22:38 -0700

    Created connection between a test and test taking page

--- app/models/candidate_answer.rb
+++ app/models/candidate_answer.rb
@@ -4,15 +4,6 @@ class CandidateAnswer < ActiveRecord::Base
 	belongs_to :question
 	belongs_to :answer
 
-	# before_save :correct?
-
-	# def correct?
-	# 	if self.test_result_id && self.question_id && self.answer_id
-	# 		self.correct = true
-	# 	else
-	# 		self.correct = false
-	# 	end
-	# 	correct.save
-	# end
+	validates :email, uniqueness: true, format: { with: /.+@.+\..{2,}/i, :message => "Enter a valid Email address !" }
 
 end
